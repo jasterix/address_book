9dc2570517593f45cc0103ea2e3654690b8bc3ab
Jasterix <jasterix@users.noreply.github.com>
2023-01-25 17:16:50 -0500

    Create Db context for Contact model

--- contactsApi/Models/ContactContext.cs
+++ contactsApi/Models/ContactContext.cs
@@ -1,8 +1,12 @@
-using Microsoft.EntityFrameworkCore.DbContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace contactsApi.Models;
 
 public class ContactContext: DbContext
 {
+    public ContactContext(DbContextOptions<ContactContext> options): base(options)
+    {
 
+    }
+    public DbSet<Contact> Contacts {get; set}=null!;
 }

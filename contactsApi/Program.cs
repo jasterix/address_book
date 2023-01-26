2adb7d61659e25975fcf5959522c5cefd8dea8c9
Jasterix <jasterix@users.noreply.github.com>
2023-01-25 20:08:05 -0500

    Add AddDbContext to DI container

--- contactsApi/Program.cs
+++ contactsApi/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ContactContext>(opt => opt.UseInMemoryDatabase("Contacts"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

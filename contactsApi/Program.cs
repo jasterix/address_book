20701313b58624c3a1c264340e068ece6a78d0ff
Jasterix <jasterix@users.noreply.github.com>
2023-02-02 18:39:59 -0500

    Update swagger UI base url

--- contactsApi/Program.cs
+++ contactsApi/Program.cs
@@ -19,7 +19,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Address Book API");
+                c.RoutePrefix = string.Empty;
+            });
 }
 
 app.UseHttpsRedirection();

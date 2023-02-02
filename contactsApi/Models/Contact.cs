5d14ded924fefa8940aa5646c94b6ba2d2786932
Jasterix <jasterix@users.noreply.github.com>
2023-02-02 16:42:27 -0500

    Add Created and Secret to Contact model

--- contactsApi/Models/Contact.cs
+++ contactsApi/Models/Contact.cs
@@ -3,7 +3,9 @@ namespace contactsApi.Models;
 public class Contact
 {
     public long Id {get;set;}
+    public DateTime Created {get; set;}
     public string? Name {get;set;}
     public int PhoneNumber {get;set;}
+    public string? Secret { get; set; }
 
 }

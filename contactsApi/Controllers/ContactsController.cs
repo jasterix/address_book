7e5c48d545c4ef7b3568aa2a3171b30b3584d711
Jasterix <jasterix@users.noreply.github.com>
2023-02-03 16:20:52 -0500

    Update Get to use seed data whenno contacts. Also update action names in controller

--- contactsApi/Controllers/ContactsController.cs
+++ contactsApi/Controllers/ContactsController.cs
@@ -15,30 +15,28 @@ namespace contactsApi.Controllers
 
         public ContactsController(ContactContext context, ILogger<ContactsController> logger)
         {
-            _context = context;
-            _logger = logger;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         // Get all
         // GET: api/Contacts
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        [HttpGet(Name = "GetAll")]
+        public async Task<ActionResult<List<Contact>>> GetContacts()
         {
-            if (_context.Contacts == null)
+            var total = _context.Contacts.ToListAsync().Result.Count;
+            if (!(total > 0))
             {
-                return NotFound();
+                // Seed data
+                return ContactService.GetAll();
             }
             return await _context.Contacts.ToListAsync();
         }
 
-        // [HttpGet]
-        // public ActionResult<List<Contact>> GetAll() =>
-        //     ContactService.GetAll();
-
         // CREATE
         // POST: api/Contacts
         // To protect from over-posting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
+        [HttpPost(Name = "Create")]
         public async Task<ActionResult<Contact>> PostContact(Contact contact)
         {
             if (_context.Contacts == null)
@@ -51,10 +49,29 @@ namespace contactsApi.Controllers
             return CreatedAtAction(nameof(GetContact), new { id = contact.Id }, contact);
         }
 
+        // Read
+        // GET: api/Contacts/5
+        [HttpGet("{id}", Name = "Read")]
+        public async Task<ActionResult<Contact>> GetContact(long id)
+        {
+            if (_context.Contacts == null)
+            {
+                return NotFound();
+            }
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
         // Update
         // PUT: api/Contacts/5
         // To protect from over-posting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
+        [HttpPut("{id}", Name = "Update")]
         public async Task<IActionResult> PutContact(long id, Contact contact)
         {
             if (id != contact.Id)
@@ -83,48 +100,9 @@ namespace contactsApi.Controllers
             return NoContent();
         }
 
-        // Alternate way of formatting actions 
-        // [HttpPost]
-        // public IActionResult CreateContact(CreateContactRequest request)
-        // {
-        //     var contact = new Contact(
-        //         Guid.NewGuid(),
-        //         request.Created,
-        //         request.Name
-        //     );
-
-        // var response = new ContactResponse(
-        //     contactsApi.Id,
-        //     contactsApi.name
-        // );
-
-        //     return CreatedAtAction(
-        //     ActionNameAttribute: nameOf(GetContact),
-        //     routeValues: new { id= contactsApi.Id},
-        //     value: response
-        // );
-        // }
-
-        // GET: api/Contacts/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Contact>> GetContact(long id)
-        {
-            if (_context.Contacts == null)
-            {
-                return NotFound();
-            }
-            var contact = await _context.Contacts.FindAsync(id);
-
-            if (contact == null)
-            {
-                return NotFound();
-            }
-
-            return contact;
-        }
 
         // DELETE: api/Contacts/5
-        [HttpDelete("{id}")]
+        [HttpDelete("{id}", Name = "Delete")]
         public async Task<IActionResult> DeleteContact(long id)
         {
             if (_context.Contacts == null)
@@ -140,7 +118,7 @@ namespace contactsApi.Controllers
             _context.Contacts.Remove(contact);
             await _context.SaveChangesAsync();
 
-            return NoContent();
+            return Content("User {id} has been deleted");
         }
 
         private bool ContactExists(long id)
@@ -149,3 +127,26 @@ namespace contactsApi.Controllers
         }
     }
 }
+
+
+// Alternate way of formatting actions 
+// [HttpPost]
+// public IActionResult CreateContact(CreateContactRequest request)
+// {
+//     var contact = new Contact(
+//         Guid.NewGuid(),
+//         request.Created,
+//         request.Name
+//     );
+
+// var response = new ContactResponse(
+//     contactsApi.Id,
+//     contactsApi.name
+// );
+
+//     return CreatedAtAction(
+//     ActionNameAttribute: nameOf(GetContact),
+//     routeValues: new { id= contactsApi.Id},
+//     value: response
+// );
+// }

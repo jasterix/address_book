fe4e9632b3beafd14ecedaafe62ade6fa639d7b9
Jasterix <jasterix@users.noreply.github.com>
2023-02-03 17:40:35 -0500

    Remove secret

--- contactsApi/Models/Contact.cs
+++ contactsApi/Models/Contact.cs
@@ -6,6 +6,4 @@ public class Contact
     public DateTime Created { get; set; }
     public string? Name { get; set; }
     public string? PhoneNumber { get; set; }
-    // public string? Secret { get; set; }
-
 }

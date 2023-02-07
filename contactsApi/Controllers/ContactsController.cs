90c482248a411397a33224d85ce8ad6bf9c1693c
Jasterix <jasterix@users.noreply.github.com>
2023-02-06 23:20:41 -0500

    Update Put operation to allow for updating one field at a time and prevent overriding other fields with null. Clean up controller code

--- contactsApi/Controllers/ContactsController.cs
+++ contactsApi/Controllers/ContactsController.cs
@@ -37,25 +37,17 @@ namespace contactsApi.Controllers
             {
                 if (_context.Contacts == null)
                 {
-                    return Problem("Entity set 'ContactContext.Contacts'  is null.");
+                    _logger.LogError("Invalid contact object sent from client.");
+                    return Problem("Contact object is null.");
                 }
-
-                else if (contact.Name is null || contact.PhoneNumber is null)
-                {
-                    _logger.LogError("All fields ae required");
-                    return BadRequest("All fields ae required");
-                }
-
                 _context.Contacts.Add(contact);
                 await _context.SaveChangesAsync();
-
                 return CreatedAtAction(nameof(GetContact), new { id = contact.Id }, contact);
             }
             catch (Exception exception)
             {
                 _logger.LogError($"Something went wrong inside UpdateOwner action: {exception.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                "Error creating new contact record");
+                return NotFound();
             }
         }
 
@@ -64,56 +56,66 @@ namespace contactsApi.Controllers
         [HttpGet("{id}", Name = "Read")]
         public async Task<ActionResult<Contact>> GetContact(long id)
         {
-            if (_context.Contacts == null)
+            try
             {
-                return NotFound();
-            }
-            var contact = await _context.Contacts.FindAsync(id);
+                if (_context.Contacts == null)
+                {
+                    return NotFound();
+                }
+                var contact = await _context.Contacts.FindAsync(id);
 
-            if (contact == null)
+                if (contact == null)
+                {
+                    return NotFound();
+                }
+                return contact;
+            }
+            catch (Exception exception)
             {
-                return NotFound();
+                _logger.LogError($"Something went wrong inside UpdateOwner action: {exception.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error finding contact.");
             }
-
-            return contact;
         }
 
         // Update
         // PUT: api/Contacts/5
         // To protect from over-posting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}", Name = "Update")]
-        public async Task<IActionResult> PutContact(long id, [FromBody] Contact contact)
+        public async Task<IActionResult> PutContact(long id, [FromBody] Contact update)
         {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (id != contact.Id)
+            {
+                _logger.LogError("Id not found.");
+                return BadRequest("Employee ID mismatch");
+            }
+            if (update.Name != null)
+            {
+                contact.Name = update.Name;
+            }
+            if (update.PhoneNumber != null)
+            {
+                contact.PhoneNumber = update.PhoneNumber;
+            }
+
             try
             {
-                if (id != contact.Id)
-                {
-                    return Content("Employee ID mismatch");
-                }
-
-                if (contact is null)
-                {
-                    _logger.LogError("Contact object sent from client is null.");
-                    return BadRequest("Contact object is null");
-                }
-                if (!ModelState.IsValid)
-                {
-                    _logger.LogError("Invalid contact object sent from client.");
-                    return BadRequest("Invalid model object");
-                }
-
-                _context.Entry(contact).State = EntityState.Modified;
-
-
                 await _context.SaveChangesAsync();
             }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                "Error updating data");
-            }
 
-            return NoContent();
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok(contact);
         }
 
 
@@ -121,24 +123,24 @@ namespace contactsApi.Controllers
         [HttpDelete("{id}", Name = "Delete")]
         public async Task<IActionResult> DeleteContact(long id)
         {
-            if (_context.Contacts == null)
+            try
             {
-                return NotFound();
-            }
-            var contact = await _context.Contacts.FindAsync(id);
-            if (contact == null)
-            {
-                return NotFound();
-            }
-            var stateBeforeAdd = _context.Entry(contact).State;
-            _context.Add(contact);
-            var stateAfterAdd = _context.Entry(contact).State;
-            _context.SaveChanges();
-            var stateAfterSaveChanges = _context.Entry(contact).State;
-            _context.Contacts.Remove(contact);
-            await _context.SaveChangesAsync();
+                if (_context.Contacts == null)
+                {
+                    return NotFound();
+                }
+                var contact = await _context.Contacts.FindAsync(id);
+                _context.Contacts.Remove(contact);
+                await _context.SaveChangesAsync();
 
-            return Content("User ${id} has been deleted");
+                return Content("User ${contact.Name} has been deleted");
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError($"Something went wrong inside UpdateOwner action: {exception.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error finding contact.");
+            }
         }
 
         private bool ContactExists(long id)
@@ -147,26 +149,3 @@ namespace contactsApi.Controllers
         }
     }
 }
-
-
-// Alternate way of formatting actions 
-// [HttpPost]
-// public IActionResult CreateContact(CreateContactRequest request)
-// {
-//     var contact = new Contact(
-//         Guid.NewGuid(),
-//         request.Created,
-//         request.Name
-//     );
-
-// var response = new ContactResponse(
-//     contactsApi.Id,
-//     contactsApi.name
-// );
-
-//     return CreatedAtAction(
-//     ActionNameAttribute: nameOf(GetContact),
-//     routeValues: new { id= contactsApi.Id},
-//     value: response
-// );
-// }
